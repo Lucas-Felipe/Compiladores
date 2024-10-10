23bb540b5b07aa3010a8329248bfe78bb3a2851f
Lucas Felipe <l17.lucas@gmail.com>
2024-10-10 21:32:37 +0000

    commit inicial, j[a com trabalho final

---
+++ trabalho1/Debug/CMakeFiles/tradutor.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/eu/Desktop/lab01compiladores/error.cpp" "CMakeFiles/tradutor.dir/error.cpp.o" "gcc" "CMakeFiles/tradutor.dir/error.cpp.o.d"
+  "/home/eu/Desktop/lab01compiladores/lexer.cpp" "CMakeFiles/tradutor.dir/lexer.cpp.o" "gcc" "CMakeFiles/tradutor.dir/lexer.cpp.o.d"
+  "/home/eu/Desktop/lab01compiladores/parser.cpp" "CMakeFiles/tradutor.dir/parser.cpp.o" "gcc" "CMakeFiles/tradutor.dir/parser.cpp.o.d"
+  "/home/eu/Desktop/lab01compiladores/tradutor.cpp" "CMakeFiles/tradutor.dir/tradutor.cpp.o" "gcc" "CMakeFiles/tradutor.dir/tradutor.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
