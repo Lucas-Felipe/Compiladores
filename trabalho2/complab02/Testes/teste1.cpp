24ed4e8a124f9498be8a483d9bb5e4c36f1485b4
Lucas Felipe <l17.lucas@gmail.com>
2024-10-22 19:39:30 +0000

    trabalho 2

---
+++ trabalho2/complab02/Testes/teste1.cpp
@@ -0,0 +1,19 @@
+/* exemplo de programa em 
+   linguagem simplificada */
+   
+int main()
+{
+  // variáveis
+  int i;
+  int j;
+  bool z;
+  bool a;
+  bool b;
+
+  // atribuições
+  i = 15;
+  j = 30;
+  z = i > 10 && j <= 20;
+  a = 6;
+  b = 0;
+}
