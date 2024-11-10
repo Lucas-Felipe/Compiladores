ddbb4c8f2590a3aff86b9f7465ac56a1bb9aba27
Lucas Felipe <l17.lucas@gmail.com>
2024-11-10 19:37:30 +0000

    trabalho 4

---
+++ trabalho4/Words/CMakeFiles/words.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/words.dir/lex.yy.cc.o"
+  "CMakeFiles/words.dir/lex.yy.cc.o.d"
+  "lex.yy.cc"
+  "words"
+  "words.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/words.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
